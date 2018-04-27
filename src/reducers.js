451d38a4d9528fb27f05381f1b520ed04e097838
Cheng Yeh Lee <chengyeh@ku.edu>
2018-04-27 05:16:20 -0500

    Added the reducer that updates the requestRobots state

--- src/reducers.js
+++ src/reducers.js
@@ -1,13 +1,36 @@
-import { CHANGE_SEARCH_FIELD } from './constants';
+import { 
+	CHANGE_SEARCH_FIELD,
+	REQUEST_API_PENDING,
+	REQUEST_API_SUCCESS,
+	REQUEST_API_FAILED } from './constants';
 
-const initialState = {
+const initialStateSearchRobots = {
 	searchfield: ''
 };
 
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state=initialStateSearchRobots, action={}) => {
 	switch (action.type) {
 		case CHANGE_SEARCH_FIELD:
 			return {...state, searchfield: action.payload};
+		default:
+			return state;
+	}
+};
+
+const initialStateRequestRobots = {
+	isPending: false,
+	error: '',
+	robots: []
+};
+
+export const requestRobots = (state=initialStateRequestRobots, action={}) => {
+	switch (action.type) {
+		case REQUEST_API_PENDING:
+			return {...state, isPending: true};
+		case REQUEST_API_SUCCESS:
+			return {...state, isPending: false, robots: action.payload};
+		case REQUEST_API_FAILED:
+			return {...state, isPending:false, error: action.payload};
 		default:
 			return state;
 	}
