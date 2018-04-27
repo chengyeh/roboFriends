9cbee85d478332fba7fb8c6c7537f1c24bebf245
Cheng Yeh Lee <chengyeh@ku.edu>
2018-04-27 05:56:15 -0500

    Mapped state and action dispatch to the props

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,51 +5,34 @@ import CardList from '../components/CardList';
 import SearchBar from '../components/SearchBar';
 import Scroll from '../components/Scroll';
 import './App.css';
-import { setSearchField } from '../actions';
+import { setSearchField, requestAPI } from '../actions';
 
 const mapStateToProps = state => ({
-	searchfield: state.searchRobots.searchfield
+	searchfield: state.searchRobots.searchfield,
+	isPending: state.requestRobots.isPending,
+	error: state.requestRobots.error,
+	robots: state.requestRobots.robots
 });
 
 const mapDispatchToProps = dispatch => ({
-	onSearchChange: event => dispatch(setSearchField(event.target.value))
+	onSearchChange: event => dispatch(setSearchField(event.target.value)),
+	getRobots: () => dispatch(requestAPI())
 });
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-		};
-	}
 
-	async  componentDidMount() {
-		// fetch('https://jsonplaceholder.typicode.com/users')
-		// .then(response => response.json())
-		// .then(users => this.setState({robots: users}));
-
-		try {
-			const response = await fetch('https://jsonplaceholder.typicode.com/users');
-			const data = await response.json();
-
-			if(data.constructor !== Array) {
-				throw new Error('Invalid return data type.');
-			}
-			this.setState({robots: data})
-		} catch(err) {
-			console.log(err.message);
-		}
+	componentDidMount() {
+		this.props.getRobots();
 	}
 
 	render() {
-		const { robots } = this.state;
-		const { searchfield, onSearchChange } = this.props;
+		const { searchfield, onSearchChange, robots, isPending } = this.props;
 		const filteredRobots = robots.filter((robot) => {
 			return robot.name.toLowerCase().includes(searchfield.toLowerCase()) ||
 			robot.email.toLowerCase().includes(searchfield.toLowerCase());
 		});
 
-		return (!robots.length ? 
+		return (isPending ? 
 			<h1 className='tc f1'>Loading...</h1> :
 			<div className='tc'>
 				<h1 className='f1'>RoboFriends</h1>
