f2be42490470aff0792282d61684c4072687f33d
Cheng Yeh Lee <chengyeh@ku.edu>
2018-04-27 05:15:22 -0500

    Added action that asynchronously fetch data from the API to get robots

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,18 @@
-import { CHANGE_SEARCH_FIELD } from './constants';
+import { 
+	CHANGE_SEARCH_FIELD,
+	REQUEST_API_PENDING,
+	REQUEST_API_SUCCESS,
+	REQUEST_API_FAILED } from './constants';
 
 export const setSearchField = (text) => ({
 	type: CHANGE_SEARCH_FIELD,
 	payload: text
 });
+
+export const requestAPI = () => (dispatch) => {
+	dispatch({type: REQUEST_API_PENDING});
+	fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(user => dispatch({type: REQUEST_API_SUCCESS, payload: user}))
+		.catch(error => dispatch({type: REQUEST_API_FAILED, payload: error}));
+};
